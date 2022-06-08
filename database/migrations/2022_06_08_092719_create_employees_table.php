9705771c4af3582442f66c3b32ec4e87bd117afd
singok <singoksuraj@gmail.com>
2022-06-08 15:16:25 +0545

    Employee Migration Created

---
+++ database/migrations/2022_06_08_092719_create_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname');
+            $table->enum('gender', ['Male', 'Female', 'Other']);
+            $table->integer('age');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('postcode');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+};
